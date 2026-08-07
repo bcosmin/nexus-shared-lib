f61d2092e960d839e711a6ca94bba90d6e7892d7
Cosmin Bilan <bilancosmin@gmail.com>
2026-08-08 02:23:41 +0300

    fix logger for string

--- src/com/nexus/Logger.groovy
+++ src/com/nexus/Logger.groovy
@@ -13,21 +13,22 @@ class Logger implements Serializable {
     }
 
     void info(String message) {
-        steps.echo "[INFO] '\u2139\ufe0f' ${message}"
+        steps.echo "[INFO] '\u2139\ufe0f' ${message}.toString()"
     }
 
 
     void warn(String message) {
-        steps.echo "[WARN] ⚠️ ${message}"
+        steps.echo "[WARN] ⚠️ ${message}.toString()"
+        step.currentBuild.result = 'WARRNING'
     }
 
     void success(String message) {
-        steps.echo "[SUCCESS] ✅ ${message}"
+        steps.echo "[SUCCESS] ✅ ${message}.toString()"
         step.currentBuild.result = 'SUCCESS'
     }
 
     void error(String message) {
-        steps.echo "[ERROR] ❌ ${message}"
+        steps.echo "[ERROR] ❌ ${message}.toString()"
         step.currentBuild.result = 'FAILURE'
     }
 }
